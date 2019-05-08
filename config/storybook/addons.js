bb5c5f673dc2a566d208e6e0c19af0e5c443e574
ayiaq1 <250751717@qq.com>
2019-05-08 09:58:12 +0800

    在线测试

--- config/storybook/addons.js
+++ config/storybook/addons.js
@@ -3,10 +3,10 @@
  * @Author: dawdler
  * @LastModifiedBy: dawdler
  * @Date: 2019-03-27 15:07:32
- * @LastEditTime: 2019-03-27 15:23:54
+ * @LastEditTime: 2019-05-08 09:52:39
  */
 /* eslint-disable import/no-extraneous-dependencies */
-import '@storybook/addon-actions/register';
 import '@storybook/addon-knobs/register';
+import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-notes/register';
